6fe2bd9a4cacdaaaf1960289a4bfa640e55a69f9
Ducky <kriperoka@gmail.com>
2024-03-03 15:45:21 +0300

    launch fix

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-"C:\Users\kripe\AppData\Local\Programs\Opera GX\launcher.exe" "http://127.0.0.1:1880/"
+"C:\Users\Platonova\AppData\Local\Programs\Opera GX\launcher.exe" "http://127.0.0.1:1880/"
 node-red -u ./node-red
